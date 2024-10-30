d818b1ab2f15960b0fc6dc428fd6f3225974bda1
Saurav Pant <pantsaurav005@gmail.com>
2024-10-30 16:46:40 +0530

    Fix: Volunteer page prod Fixed

--- client/src/app/api/Volunteertemp/route.ts
+++ client/src/app/api/Volunteertemp/route.ts
@@ -1,43 +1,43 @@
 import { NextResponse } from 'next/server';
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 
-export async function POST(req: Request) {
-    try {
-        const body = await req.json();
-        const { name, email, phone, college_work,duration,address } = body;
+// export async function POST(req: Request) {
+//     try {
+//         const body = await req.json();
+//         const { name, email, phone, college_work,duration,address } = body;
 
-        if (!name || !email || !phone || !address || !college_work || !duration) {
-            return NextResponse.json(
-                { error: "All the details are required" },
-                { status: 400 }
-            );
-        }
+//         if (!name || !email || !phone || !address || !college_work || !duration) {
+//             return NextResponse.json(
+//                 { error: "All the details are required" },
+//                 { status: 400 }
+//             );
+//         }
 
-        const contactMessage = await prisma.contactMessage.create({
-            data: {
-                name,
-                email,
-                phone,
-                college_work,
-                duration,
-                address,
-            },
-        });
+//         const contactMessage = await prisma.contactMessage.create({
+//             data: {
+//                 name,
+//                 email,
+//                 phone,
+//                 college_work,
+//                 duration,
+//                 address,
+//             },
+//         });
 
-        return NextResponse.json(
-            { message: "Details submitted successfully", contactMessage },
-            { status: 200 }
-        );
-    } catch (error) {
-        console.error("Error processing donation:", error);
-        return NextResponse.json(
-            { error: "Internal server error" },
-            { status: 500 }
-        );
-    } finally {
-        await prisma.$disconnect();
-    }
-}
+//         return NextResponse.json(
+//             { message: "Details submitted successfully", contactMessage },
+//             { status: 200 }
+//         );
+//     } catch (error) {
+//         console.error("Error processing donation:", error);
+//         return NextResponse.json(
+//             { error: "Internal server error" },
+//             { status: 500 }
+//         );
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// }
 
 export async function GET() {
     return NextResponse.json({ message: "Hello from VolunteerTemp API" });
